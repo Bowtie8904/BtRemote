137d7d36f9fa9766c37bbf4f043d1659e45a2348
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2021-09-30 10:07:55 +0200

    Added supertype.

--- src/bt/remote/socket/evnt/ReconnectStarted.java
+++ src/bt/remote/socket/evnt/ReconnectStarted.java
@@ -6,32 +6,13 @@ import bt.remote.socket.Client;
  * @author &#8904
  *
  */
-public class ReconnectStarted
+public class ReconnectStarted extends ClientEvent
 {
-    private Client client;
-
     /**
      * @param client
      */
     public ReconnectStarted(Client client)
     {
-        this.client = client;
-    }
-
-    /**
-     * @return the client
-     */
-    public Client getClient()
-    {
-        return this.client;
-    }
-
-    /**
-     * @param client
-     *            the client to set
-     */
-    public void setClient(Client client)
-    {
-        this.client = client;
+        super(client);
     }
 }
